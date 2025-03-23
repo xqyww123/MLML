e75981f1363a5f9d119a43c966441d519e9d2649
Qiyuan Xu <xqyww123@gmail.com>
2025-03-23 16:47:20 +0800

    Increase Jave mem

--- tools/Build_AFP_Image/build_dep1.sh
+++ tools/Build_AFP_Image/build_dep1.sh
@@ -8,6 +8,6 @@ isabelle components -u ./contrib/afp-2025-02-12/thys
 NPROC=$(nproc)
 
 echo "Building AFP-DEP1. This can take 30+ hours"
-export ISABELLE_TOOL_JAVA_OPTIONS="-Xms4g -Xmx32g -Xss16m"
+export ISABELLE_TOOL_JAVA_OPTIONS="-Xms4g -Xmx64g -Xss64m"
 isabelle build -b -D ./tools/Build_AFP_Image/AFP-DEP1 -o threads=$NPROC AFP-DEP1
 
