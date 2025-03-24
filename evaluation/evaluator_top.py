03fb1f7ec68b54bd68f4f40acf03a08191ce28c5
Qiyuan Xu <xqyww123@gmail.com>
2025-03-24 14:17:49 +0800

    Clean

--- evaluation/evaluator_top.py
+++ evaluation/evaluator_top.py
@@ -6,7 +6,7 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
         logging.StreamHandler()
     ]
