84f645b224032bad0deb78bf3ecc35e550994f0c
haonan-li <nathan.8270.n@gmail.com>
2025-03-31 15:16:32 +0400

    script

---
+++ apply_nodes.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import time
+import subprocess
+import threading
+
+N = 24
+node_list = [f"cn-{i:02d}" for i in range(N)]
+
+
+def check_node(node):
+    # Get list of running jobs for the user
+    cmd = f"squeue -u haonan.li"
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    if node in result.stdout.strip():
+        return True
+    return False
+
+
+def alloc_servers(node):
+    while True:
+        if not check_node(node):
+            cmd = f"srun --job-name=myjob --partition=standard --nodes=1 --nodelist={node} --ntasks-per-node=1 --cpus-per-task=64 --time=120:00:00 sleep 10000000000"
+            subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        time.sleep(1)
+
+
+# Create and start threads for each node
+threads = []
+for node in node_list:
+    thread = threading.Thread(target=alloc_servers, args=(node,))
+    thread.start()
+    threads.append(thread)
+
+# Wait for all threads to complete (which won't happen in this case due to infinite loops)
+for thread in threads:
+    thread.join()
---
+++ bc.py
@@ -0,0 +1,14 @@
+import os
+
+getjobs_cmd = 'squeue --format="%.18i %.9P %.30j %.8u %.8T %.10M %.9l %.6D %R" --me | grep "miniLang" > tmp.txt'
+os.system(getjobs_cmd)
+
+with open('tmp.txt','r') as f:
+    for line in f.readlines():
+        pid = line.strip().split()[0]
+        cancel_cmd = 'scancel ' + pid
+        os.system(cancel_cmd)
+
+os.system('rm tmp.txt')
+# os.system("rm results/* logs/* tasks/*")
+# os.system("rm -r translation_tmp/*")
