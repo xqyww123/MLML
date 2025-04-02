e60677a492ac5268d9aa804a7ccdc7cd92d78322
Qiyuan Xu <xqyww123@gmail.com>
2025-04-02 12:35:53 +0800

    Use AFP-1-PISA!

--- init.sh
+++ init.sh
@@ -5,24 +5,18 @@ echo "Initializing system..."
 git pull
 git submodule update --init --recursive --remote
 pip install -q gdown
+pip install -q isarepl --upgrade
 mkdir -p ./cache/downloads
 mkdir -p ./translation/results
 mkdir -p ./cache/translation/tmp
 
 source ./envir.sh
 
-if [ ! -d "./contrib/Isabelle2024" ]; then
-    echo "Downloading Isabelle2024"
-    gdown --fuzzy https://drive.google.com/file/d/1-cdF3SZoqtlhWTbqlgMT9h4S5T8LRvLK/view?usp=sharing -O ./cache/downloads/Isabelle2024_linux.tar.gz
-    echo "Unpacking Isabelle2024"
-    tar -xzf ./cache/downloads/Isabelle2024_linux.tar.gz -C ./contrib
-fi
-
-if [ ! -d "./contrib/afp-2025-02-12" ]; then
-    echo "Downloading AFP"
-    gdown --fuzzy https://drive.google.com/file/d/1_iuUpTg-AsahixhIJkmBOlFGsnzvoovx/view?usp=sharing -O ./cache/downloads/afp-2025-02-12.tar.gz
-    echo "Unpacking AFP"
-    tar -xzf ./cache/downloads/afp-2025-02-12.tar.gz -C ./contrib
+if [ ! -d "./contrib/Isabelle2024" ] || [ ! -d "./contrib/afp-2025-02-12" ]; then
+    echo "Downloading Isabelle2024 and AFP"
+    gdown --fuzzy https://drive.google.com/file/d/176tufd_eHxzpAHdVV5-XMJSRy8NVaGq9/view?usp=sharing -O ./cache/downloads/Isabelle2024_and_afp-2025-02-12.tar.gz
+    echo "Unpacking Isabelle2024 and AFP"
+    tar -xzf ./cache/downloads/Isabelle2024_and_afp-2025-02-12.tar.gz -C ./contrib
 fi
 
 rm -f  $(isabelle getenv -b ISABELLE_HOME_USER)/etc/components 2>/dev/null
