a9c306493f8d01e800412e6f98a486edf0f64158
Qiyuan Xu <xqyww123@gmail.com>
2025-04-06 21:23:52 +0800

    revert

--- tools/slurm.py
+++ tools/slurm.py
@@ -28,22 +28,22 @@ def restart_job(node):
             cmd = f"scancel {job_id}"
             os.system(cmd)
     time.sleep(60)
-    alloc_server(node)
-    time.sleep(10)
+    # alloc_server(node)
+    # time.sleep(10)
 
-#def alloc_server(node):
-#    while True:
-#        if not check_node(node):
-#            cmd = f"srun --job-name=minilang --partition=standard --nodes=1 --nodelist={node} --ntasks-per-node=1 --cpus-per-task=128 --time=120:00:00 sleep 10000000000"
-#            subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-#        time.sleep(10)
+def alloc_server(node):
+    while True:
+        if not check_node(node):
+            cmd = f"srun --job-name=minilang --partition=standard --nodes=1 --nodelist={node} --ntasks-per-node=1 --cpus-per-task=128 --time=120:00:00 sleep 10000000000"
+            subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        time.sleep(10)
 
 
 def alloc_servers(node_list):
     for node in node_list:
-        alloc_server(node)
-        #thread = threading.Thread(target=alloc_server, args=(node,))
-        #thread.start()
+        #alloc_server(node)
+        thread = threading.Thread(target=alloc_server, args=(node,))
+        thread.start()
 
 
 def free_servers():
