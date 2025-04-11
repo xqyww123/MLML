14408e6f6f4b16bbed7d8b2d8a6cba829df54c41
Qiyuan Xu <xqyww123@gmail.com>
2025-04-11 16:22:17 +0800

    MLML interfaces

--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -6,7 +6,7 @@ from Isa_Mini import Mini
 import csv
 import logging
 from enum import Enum
-from data.isabelle import prelude_of, PISA_DATA, get_ISAR_PROOFS, get_MINIF2F_VALIDATION, get_MINIF2F_TEST
+from data.isabelle import CaseNotAvailable, PISA_Data, get_MINIF2F_VALIDATION, get_MINIF2F_TEST, MiniF2F_Data, AFP_Data
 from sqlitedict import SqliteDict
 import threading
 import concurrent.futures
@@ -32,7 +32,6 @@ class Status(Enum):
     SUCCESS = "SUCCESS"
     FAIL = "FAIL"
     CASE_NOT_AVAILABLE = "CASE_NOT_AVAILABLE"
-    NOT_FINISHED = "NOT_FINISHED"
 
 class Result:
     def __init__(self, status : Status, error : Exception | None):
@@ -56,14 +55,6 @@ class Case:
                     ret.append(Case(data["index"], data["response"]))
         return ret
 
-
-class CaseNotAvailable(Exception):
-    """Exception raised when a test case is not available."""
-    def __init__(self, idx, message="The requested test case is not available"):
-        self.idx = idx
-        self.message = message
-        super().__init__(self.message)
-
 class Evaluator:
     def __enter__(self):
         return self
@@ -74,7 +65,7 @@ class Evaluator:
     def all_cases(self): # -> enumerate[Index]:
         raise NotImplementedError("all_cases must be implemented by subclass")
 
-    def validate(self, index, srcs : enumerate[str]) -> Result:
+    def validate(self, index, src : str) -> Result:
         raise NotImplementedError("validate must be implemented by subclass")
     
     def start_case(self, index) -> None:
@@ -100,23 +91,24 @@ class MiniLang_Base(Evaluator):
             self.mini.__exit__(exc_type, exc_value, traceback)
         super().__exit__(exc_type, exc_value, traceback)
         return None
+
+    def close(self):
+        if self.mini:
+            self.mini.close()
     
-    def validate(self, index, srcs):
+    def validate(self, index, src):
         try:
             self.start_case(index)
         except CaseNotAvailable:
             return Result(Status.CASE_NOT_AVAILABLE, None)
-        result = Result(Status.FAIL, None)
-        for src in srcs:
-            try:
-                _, finished = self.mini.eval(src, self.timeout)
-                if finished:
-                    result = Result(Status.SUCCESS, None)
-                    break
-            except REPLFail as E:
-                result = Result(Status.FAIL, E)
-                pass
-        return result
+        try:
+            _, finished = self.mini.eval(src, self.timeout)
+            if finished:
+                return Result(Status.SUCCESS, None)
+            else:
+                return Result(Status.FAIL, "Proof not finished")
+        except REPLFail as E:
+            return Result(Status.FAIL, E)
 
     def move_to(self, file, line, column):
         file = os.path.abspath(file)
@@ -130,39 +122,65 @@ class MiniLang_Base(Evaluator):
             self.mini.close()
         self.mini = Mini(self.addr, 'HOL', ML_base_injection=False)
 
-class MiniLang_PISA(MiniLang_Base):
-    def all_cases(self):
-        return range(len(PISA_DATA))
+class MiniLang_PISA(MiniLang_Base, PISA_Data):
 
-    def start_case(self, index):
+    def __init__(self, addr):
+        MiniLang_Base.__init__(self, addr)
+        PISA_Data.__init__(self)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        PISA_Data.__exit__(self, exc_type, exc_value, traceback)
+        MiniLang_Base.__exit__(self, exc_type, exc_value, traceback)
+        return None
+    
+    def __enter__(self):
+        PISA_Data.__enter__(self)
+        MiniLang_Base.__enter__(self)
+        return self
+    
+    def close(self):
+        PISA_Data.close(self)
+        MiniLang_Base.close(self)
+
+    def start_case(self, index : int):
         """
         index is the index of the case in the PISA dataset, from 0 to 2999
         """
         try:
-            pos_before, pos, statement = PISA_DATA[index]
+            pos = self.proof_pos_of(index)
         except KeyError:
             raise CaseNotAvailable(index, f"MiniLang_PISA: case {index} not available")
         self.move_to(pos.file, pos.line, pos.column)
     
+class MiniLang_AFP(MiniLang_Base, AFP_Data):
 
-#    @classmethod
-#    def print_state(response):
-#        ret, finished = response
-#        state = Mini.parse_prooftree(ret[2])
-#        new_items = Mini.parse_item(ret[0])
-#        logger.info(json.dumps(state, indent=2))
-#        if ret[1]:
-#            logger.info('enter case ' + ret[1])
-#        if ret[0][0] or ret[0][1]:
-#            logger.info('newly introduced: ' + json.dumps(new_items, indent=2))
-#        if finished:
-#            logger.info('All goals are proven')
-#
-#    def pretty_print(self, src):
-#        MiniLang_PISA.print_state(self.eval(src))
+    def __init__(self, addr):
+        MiniLang_Base.__init__(self, addr)
+        AFP_Data.__init__(self)
 
+    def __enter__(self):
+        AFP_Data.__enter__(self)
+        MiniLang_Base.__enter__(self)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        AFP_Data.__exit__(self, exc_type, exc_value, traceback)
+        MiniLang_Base.__exit__(self, exc_type, exc_value, traceback)
+        return None
+
+    def close(self):
+        AFP_Data.close(self)
+        MiniLang_Base.close(self)
+
+    def start_case(self, index : Position):
+        try:
+            pos = self.proof_pos_of(index)
+        except KeyError:
+            raise CaseNotAvailable(index, f"MiniLang_AFP: case {index} not available")
+        self.move_to(pos.file, pos.line, pos.column)
 
 class Isar_Base(Evaluator):
+
     def __init__(self, addr):
         self.addr = addr
         self.repl = Client(addr, 'HOL')
@@ -178,6 +196,9 @@ class Isar_Base(Evaluator):
         super().__exit__(exc_type, exc_value, traceback)
         return None
 
+    def close(self):
+        self.repl.close()
+
     def move_to(self, file, line, column=0):
         self.repl.rollback("init")
         self.repl.file (os.path.abspath(file), line, column, cache_position=True, use_cache=True)
@@ -186,24 +207,21 @@ class Isar_Base(Evaluator):
         self.repl.rollback("init")
         self.repl.eval(src)
 
-    def validate(self, index, srcs):
+    def validate(self, index, src):
         try:
             self.start_case(index)
         except CaseNotAvailable:
             return Result(Status.CASE_NOT_AVAILABLE, None)
-        result = Result(Status.FAIL, None)
-        for src in srcs:
-            try:
-                response, error = self.repl.eval(src, timeout=600000)
-                if error:
-                    result = Result(Status.FAIL, error)
-                elif response and not response[-1][3][3]:
-                    result = Result(Status.SUCCESS, None)
-                    break
-            except REPLFail as E:
-                result = Result(Status.FAIL, E)
-                pass
-        return result
+        try:
+            response, error = self.repl.eval(src, timeout=600000)
+            if error:
+                return Result(Status.FAIL, error)
+            elif response and not response[-1][3][3]:
+                return Result(Status.SUCCESS, None)
+            else:
+                return Result(Status.FAIL, "Proof not finished")
+        except REPLFail as E:
+            return Result(Status.FAIL, E)
 
     def reset(self):
         if self.repl:
@@ -211,35 +229,53 @@ class Isar_Base(Evaluator):
         self.repl = Client(self.addr, 'HOL')
         self.repl.record_state("init")
 
-class Isar_PISA(Isar_Base):
-    def all_cases(self):
-        return range(len(PISA_DATA))
+class Isar_PISA(Isar_Base, PISA_Data):
+
+    def __init__(self, addr):
+        Isar_Base.__init__(self, addr)
+        PISA_Data.__init__(self)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        PISA_Data.__exit__(self, exc_type, exc_value, traceback)
+        Isar_Base.__exit__(self, exc_type, exc_value, traceback)
+        return None
+    
+    def __enter__(self):
+        PISA_Data.__enter__(self)
+        Isar_Base.__enter__(self)
+        return self
+
+    def close(self):
+        PISA_Data.close(self)
+        Isar_Base.close(self)
 
     def start_case(self, index : int):
-        """
-        index is the index of the case in the PISA dataset, from 0 to 2999
-        """
-        try:
-            pos_before, pos, statement = PISA_DATA[index]
-        except KeyError:
-            raise CaseNotAvailable(index, f"Isar_PISA: case {index} not available")
+        pos = self.proof_pos_of(index)
         self.move_to(pos.file, pos.line, pos.column)
 
-class Isar_AFP(Isar_Base):
-    def all_cases(self):
-        ISAR_PROOFS = get_ISAR_PROOFS()
-        return ISAR_PROOFS.keys()
+class Isar_AFP(Isar_Base, AFP_Data):
+
+    def __init__(self, addr):
+        Isar_Base.__init__(self, addr)
+        AFP_Data.__init__(self)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        AFP_Data.__exit__(self, exc_type, exc_value, traceback)
+        Isar_Base.__exit__(self, exc_type, exc_value, traceback)
+        return None
+    
+    def __enter__(self):
+        AFP_Data.__enter__(self)
+        Isar_Base.__enter__(self)
+        return self
+
+    def close(self):
+        AFP_Data.close(self)
+        Isar_Base.close(self)
 
     def start_case(self, index : Position):
-        """
-        index is the position of the target goal to be evaluated.
-        You could call `all_cases()` to get the positions of all available training cases.
-        """
-        ISAR_PROOFS = get_ISAR_PROOFS()
-        if index not in ISAR_PROOFS:
-            raise ValueError(f"Isar: {index} is not a training case")
-        self.move_to(index.file, index.line)
-
+        pos = self.proof_pos_of(index)
+        self.move_to(pos.file, pos.line, pos.column)
 
 #if __name__ == "__main__":
 #    logger.info('self-testing')
@@ -254,8 +290,27 @@ class Isar_AFP(Isar_Base):
 #        assert(test.validate("test", 29, ["by simp"])[0] == Result.CASE_NOT_AVAILABLE)
 #        assert(test.validate("test", 1, ["using assms unfolding echelon_form_upt_k_def by auto"])[0] == Result.SUCCESS)
 
-class MiniLang_MiniF2F(MiniLang_Base):
-    def start_case(self, index : Tuple[str, int]):
+class MiniLang_MiniF2F(MiniLang_Base, MiniF2F_Data):
+
+    def __init__(self, addr):
+        MiniLang_Base.__init__(self, addr)
+        MiniF2F_Data.__init__(self)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        MiniF2F_Data.__exit__(self, exc_type, exc_value, traceback)
+        MiniLang_Base.__exit__(self, exc_type, exc_value, traceback)
+        return None
+
+    def __enter__(self):
+        MiniF2F_Data.__enter__(self)
+        MiniLang_Base.__enter__(self)
+        return self
+    
+    def close(self):
+        MiniF2F_Data.close(self)
+        MiniLang_Base.close(self)
+
+    def start_case(self, index : Tuple[str, str]):
         category, idx = index
         if category not in ["test", "valid"]:
             raise ValueError(f"Isar_MiniF2F: only support test and valid category") 
@@ -266,8 +321,27 @@ class MiniLang_MiniF2F(MiniLang_Base):
             raise CaseNotAvailable(f"MiniLang_MiniF2F: case {index} not available")
         self.reset_eval(src)
 
-class Isar_MiniF2F(Isar_Base):
-    def start_case(self, index : Tuple[str, int]):
+class Isar_MiniF2F(Isar_Base, MiniF2F_Data):
+
+    def __init__(self, addr):
+        Isar_Base.__init__(self, addr)
+        MiniF2F_Data.__init__(self)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        MiniF2F_Data.__exit__(self, exc_type, exc_value, traceback)
+        Isar_Base.__exit__(self, exc_type, exc_value, traceback)
+        return None
+    
+    def __enter__(self):
+        MiniF2F_Data.__enter__(self)
+        Isar_Base.__enter__(self)
+        return self
+    
+    def close(self):
+        MiniF2F_Data.close(self)
+        Isar_Base.close(self)
+
+    def start_case(self, index : Tuple[str, str]):
         category, idx = index
         if category not in ["test", "valid"]:
             raise ValueError(f"Isar_MiniF2F: only support test and valid category") 
@@ -365,7 +439,7 @@ def evaluate_and_save(result_path : str, cases : list[Case], evaluator : Evaluat
                                     result = db[case.index]
                                 else:
                                     try:
-                                        result = test.validate(case.index, [case.code])
+                                        result = test.validate(case.index, case.code)
                                         db[case.index] = result
                                     except REPLFail as E:
                                         logger.error(f"REPLFail error @ {case.index}: {E}")
@@ -418,133 +492,3 @@ def evaluate_and_save(result_path : str, cases : list[Case], evaluator : Evaluat
     log_state()
     return results
 
-
-
-
-#class Request:
-#    def __init__(
-#            self, 
-#            case : Case, 
-#            evaluator : Evaluator, 
-#            callback, # : Callable[[Request, Result], None], 
-#            retry=5
-#        ): # type: ignore
-#        self.case = case
-#        self.callback = callback
-#        self.evaluator = evaluator
-#        self.retry = retry
-#
-#def evaluation_engine():
-#    task_queue = queue.Queue()
-#
-#    def eval_server(server_addr):
-#        while True:
-#            try:
-#                while True:
-#                    try:
-#                        # Get next task from queue with timeout
-#                        req : Request = task_queue.get(timeout=1)
-#                    except queue.Empty:
-#                        # No more tasks in queue
-#                        time.sleep(1)
-#                        continue
-#                    
-#                    try:
-#                        logger.info(f"Server {server_addr} evaluating {req.case.index}")
-#                        evaluator = req.evaluator(server_addr)
-#                        try:
-#                            result = evaluator.validate(req.case.index, [req.case.code])
-#                            req.callback(req, result)
-#                        except REPLFail as E:
-#                            evaluator.reset()
-#                            logger.error(f"REPLFail error @ {req.case.index}: {E}")
-#                    except ConnectionError:
-#                        logger.error(f"Fail to connect to {server_addr}. Retrying in 60 seconds...")
-#                        task_queue.put(req)
-#                        time.sleep(60)
-#                    except Exception as e:
-#                        logger.error(f"Error processing case {req.case.index}: {str(e)}")
-#                        if req.retry > 0:
-#                            req.retry -= 1
-#                            task_queue.put(req)
-#                            break
-#                        elseClassification of Failures
-#                            req.callback(Result(Status.CASE_NOT_AVAILABLE, e))
-#                    finally:
-#                        task_queue.task_done()
-#            except ConnectionRefusedError:
-#                logger.error(f"Fail to connect to {server_addr}. Retrying in 10 seconds...")
-#                time.sleep(10)
-#            except Exception as e:
-#                logger.error(f"Worker thread for server {server_addr} encountered an error: {str(e)}. Retrying in 10 seconds...")
-#                time.sleep(10)
-#        
-#    # Create and start worker threads for each server
-#    threads = []
-#    for server_addr in SERVER_INSTANCES:
-#        thread = threading.Thread(target=eval_server, args=(server_addr,))
-#        thread.daemon = True  # Make threads daemon so they exit if main thread exits
-#        threads.append(thread)
-#        thread.start()
-#        
-#    return (task_queue, threads)
-#
-#(task_queue, evaluator_threads) = evaluation_engine()
-#
-#def evaluate(case : Case, evaluator : Evaluator, retry=5) -> concurrent.futures.Future[Result]:
-#    future = concurrent.futures.Future()
-#
-#    def callback(req : Request, result : Result):
-#        if not future.done():
-#            future.set_result(result)
-#    
-#    task_queue.put(Request(case, evaluator, callback, retry=retry))
-#    return future
-#
-#def evaluate_and_save(result_path : str, cases : list[Case], evaluator : Evaluator):
-#    success = 0
-#    unavailable = 0
-#    total = 0
-#    results = {}
-#    lock = threading.Lock()
-#    futures = []
-#
-#    with SqliteDict(result_path, autocommit=True) as db:
-#        def callback(case : Case, result : Result):
-#            nonlocal success, unavailable, total, results
-#            with lock:
-#                db[case.index] = result
-#                db.commit()
-#
-#                if result.status == Status.SUCCESS:
-#                    success += 1
-#                elif result.status == Status.CASE_NOT_AVAILABLE:
-#                    unavailable += 1
-#                    
-#                total += 1
-#                results[case.index] = result
-#                
-#                success_rate = success / (total-unavailable) if total - unavailable > 0 else 0
-#                unavailable_rate = unavailable / total if total > 0 else 0
-#                logger.info(f"Success: {success_rate:.3f}, Unavailable: {unavailable_rate:.3f}")
-#
-#        for case in cases:
-#            if case.index in db and db[case.index].status != Status.CASE_NOT_AVAILABLE:
-#                results[case.index] = db[case.index]
-#                callback(case, db[case.index])
-#            else:
-#                future = evaluate(case, evaluator,  retry=5)
-#                future.add_done_callback(lambda f: callback(case, f.result()))
-#                futures.append(future)
-#
-#        for future in futures:
-#            future.result()
-#
-#    return results
-
-
-# examples of evaluation
-#   evaluate('./evaluation/isar_result.db',          cases, Isar,          "training")
-#   evaluate('./evaluation/minilang_pisa_result.db', cases, MiniLang_PISA, "test")
-#   evaluate('./evaluation/isar_pisa_result.db',     cases, Isar_PISA,     "test")
-# where cases is a list of Case objects
