4180cbb2c83f9681828a972545fab4df6519da1a
Qiyuan Xu <xqyww123@gmail.com>
2025-05-23 10:50:03 +0800

    Clean_mash

--- tools/clean_mash.sh
+++ tools/clean_mash.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source ./envir.sh
 ([ -f $(isabelle getenv -b ISABELLE_HOME_USER)/mash_state ] && mv $(isabelle getenv -b ISABELLE_HOME_USER)/mash_state $(isabelle getenv -b ISABELLE_HOME_USER)/mash_state.bak.$(date +%Y%m%d%H%M%S)) || true
+rm ./cache/repl_tmps -rf
+mkdir -p ./cache/repl_tmps
