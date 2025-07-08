d2084fb4194d88c2c8aa1bae74a94fe950d7edad
Qiyuan Xu <xqyww123@gmail.com>
2025-07-08 00:31:45 -0700

    WIP

--- evaluation/evaluator_top.py
+++ evaluation/evaluator_top.py
@@ -31,80 +31,98 @@ def clean_mash(result_path):
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         match sys.argv[1]:
+            case 'mini':
+                if len(sys.argv) != 4:
+                    print("Usage: ./evaluation/evaluator_top.py mini <proof.jsonl> <result.db>")
+                    exit(1)
+                proof_jsonl = sys.argv[2]
+                result_db = sys.argv[3]
+                cases = Case.jsonl(proof_jsonl)
+                evaluate_and_save(result_db, cases, MiniLang)
+                report_evaluation(proof_jsonl, result_db)
+            case 'isar':
+                if len(sys.argv) != 4:
+                    print("Usage: ./evaluation/evaluator_top.py mini <proof.jsonl> <result.db>")
+                    exit(1)
+                proof_jsonl = sys.argv[2]
+                result_db = sys.argv[3]
+                cases = Case.jsonl(proof_jsonl)
+                evaluate_and_save(result_db, cases, Isar)
+                report_evaluation(proof_jsonl, result_db)
             case "eval-mini-pisa":
                 clean_mash("./evaluation/minilang_pisa_result.db")
                 launch_servers()
-                cases = Case.PISA_file('./evaluation/minilang_response.jsonl')
+                cases = Case.jsonl('./evaluation/minilang_response.jsonl')
                 evaluate_and_save('./evaluation/minilang_pisa_result.db', cases, lambda addr: MiniLang_PISA(addr, SH_params="timeout = 30"))
                 report_evaluation('./evaluation/minilang_response.jsonl', './evaluation/minilang_pisa_result.db')
             case "eval-mini-DS-pisa":
                 clean_mash("./evaluation/minilang-DS_pisa_result.db")
                 launch_servers()
-                cases = Case.PISA_file('./evaluation/minilang-DS_response.jsonl')
+                cases = Case.jsonl('./evaluation/minilang-DS_response.jsonl')
                 evaluate_and_save('./evaluation/minilang-DS_pisa_result.db', cases, lambda addr: MiniLang_PISA(addr, SH_params="timeout = 30"))
                 report_evaluation('./evaluation/minilang-DS_response.jsonl', './evaluation/minilang_pisa-DS_result.db')
             case "eval-mini-hasty-pisa":
                 clean_mash("./evaluation/minilang_hasty_pisa_result.db")
                 launch_servers()
-                cases = Case.PISA_file('./evaluation/minilang_response.jsonl')
+                cases = Case.jsonl('./evaluation/minilang_response.jsonl')
                 evaluate_and_save('./evaluation/minilang_hasty_pisa_result.db', cases, lambda addr: MiniLang_PISA(addr, hasty=True, SH_params="timeout = 10"))
                 report_evaluation('./evaluation/minilang_response.jsonl', './evaluation/minilang_hasty_pisa_result.db')
             case "eval-mini-no-SH-soft-pisa":
                 clean_mash("./evaluation/minilang-no-SH-soft_pisa_result.db")
                 launch_servers()
-                cases = Case.PISA_file('./evaluation/minilang-no-SH_response.jsonl')
+                cases = Case.jsonl('./evaluation/minilang-no-SH_response.jsonl')
                 evaluate_and_save('./evaluation/minilang-no-SH-soft_pisa_result.db', cases, MiniLang_PISA)
                 report_evaluation('./evaluation/minilang-no-SH-soft_response.jsonl', './evaluation/minilang-no-SH-soft_pisa_result.db')
             case "eval-mini-DS-no-SH-pisa":
                 launch_servers()
-                cases = Case.PISA_file('./evaluation/minilang-DS-no-SH_response.jsonl')
+                cases = Case.jsonl('./evaluation/minilang-DS-no-SH_response.jsonl')
                 evaluate_and_save('./evaluation/minilang-DS-no-SH_pisa_result.db', cases, MiniLang_PISA)
                 report_evaluation('./evaluation/minilang-DS-no-SH_response.jsonl', './evaluation/minilang-DS-no-SH_pisa_result.db')
             case "eval-mini-no-SH-pisa":
                 launch_servers()
-                cases = Case.PISA_file('./evaluation/minilang-no-SH_response.jsonl')
+                cases = Case.jsonl('./evaluation/minilang-no-SH_response.jsonl')
                 evaluate_and_save('./evaluation/minilang-no-SH_pisa_result.db', cases, MiniLang_PISA)
                 report_evaluation('./evaluation/minilang-no-SH_response.jsonl', './evaluation/minilang-no-SH_pisa_result.db')
             case "eval-isar-SH*-pisa":
                 clean_mash("./evaluation/isar-SH*_pisa_result.db")
                 launch_servers()
-                cases = Case.PISA_file('./evaluation/isar-SH*_response.jsonl')
+                cases = Case.jsonl('./evaluation/isar-SH*_response.jsonl')
                 evaluate_and_save('./evaluation/isar-SH*_pisa_result.db', cases, lambda addr: Isar_PISA(addr, libs=['Auto_Sledgehammer.Auto_Sledgehammer']))
                 report_evaluation('./evaluation/isar-SH*_response.jsonl', './evaluation/isar-SH*_pisa_result.db')
             case "eval-isar-SH*-DS-pisa":
                 clean_mash("./evaluation/isar-SH*-DS_pisa_result.db")
                 launch_servers()
-                cases = Case.PISA_file('./evaluation/isar-SH*-DS_response.jsonl')
+                cases = Case.jsonl('./evaluation/isar-SH*-DS_response.jsonl')
                 evaluate_and_save('./evaluation/isar-SH*-DS_pisa_result.db', cases, lambda addr: Isar_PISA(addr, libs=['Auto_Sledgehammer.Auto_Sledgehammer']))
                 report_evaluation('./evaluation/isar-SH*-DS_response.jsonl', './evaluation/isar-SH*-DS_pisa_result.db')
             case "eval-isar-pisa":
                 launch_servers()
-                cases = Case.PISA_file('./evaluation/isar_response.jsonl')
+                cases = Case.jsonl('./evaluation/isar_response.jsonl')
                 evaluate_and_save('./evaluation/isar_pisa_result.db', cases, Isar_PISA)
                 report_evaluation('./evaluation/isar_response.jsonl', './evaluation/isar_pisa_result.db')
             case "eval-before_elim_pronouns_connectives":
                 launch_servers()
-                cases = Case.PISA_file('./evaluation/before_elim_pronouns_connectives_response.jsonl')
+                cases = Case.jsonl('./evaluation/before_elim_pronouns_connectives_response.jsonl')
                 evaluate_and_save('./evaluation/before_elim_pronouns_connectives_pisa_result.db', cases, Isar_PISA)
                 report_evaluation('./evaluation/before_elim_pronouns_connectives_response.jsonl', './evaluation/before_elim_pronouns_connectives_pisa_result.db')
             case "eval-after_elim_pronouns_connectives":
                 launch_servers()
-                cases = Case.PISA_file('./evaluation/after_elim_pronouns_connectives_response.jsonl')
+                cases = Case.jsonl('./evaluation/after_elim_pronouns_connectives_response.jsonl')
                 evaluate_and_save('./evaluation/after_elim_pronouns_connectives_pisa_result.db', cases, Isar_PISA)
                 report_evaluation('./evaluation/after_elim_pronouns_connectives_response.jsonl', './evaluation/after_elim_pronouns_connectives_pisa_result.db')
             case "eval-before_split_proof":
                 launch_servers()
-                cases = Case.PISA_file('./evaluation/before_split_proof_response.jsonl')
+                cases = Case.jsonl('./evaluation/before_split_proof_response.jsonl')
                 evaluate_and_save('./evaluation/before_split_proof_pisa_result.db', cases, Isar_PISA)
                 report_evaluation('./evaluation/before_split_proof_response.jsonl', './evaluation/before_split_proof_pisa_result.db')
             case "eval-after_split_proof":
                 launch_servers()
-                cases = Case.PISA_file('./evaluation/after_split_proof_response.jsonl')
+                cases = Case.jsonl('./evaluation/after_split_proof_response.jsonl')
                 evaluate_and_save('./evaluation/after_split_proof_pisa_result.db', cases, lambda addr: Isar_PISA(addr, libs=['Minilang_Translator.MS_Translator']))
                 report_evaluation('./evaluation/after_split_proof_response.jsonl', './evaluation/after_split_proof_pisa_result.db')
             case "eval-isar-DS-pisa":
                 launch_servers()
-                cases = Case.PISA_file('./evaluation/isar-DS_response.jsonl')
+                cases = Case.jsonl('./evaluation/isar-DS_response.jsonl')
                 evaluate_and_save('./evaluation/isar-DS_pisa_result.db', cases, Isar_PISA)
                 report_evaluation('./evaluation/isar-DS_response.jsonl', './evaluation/isar-DS_pisa_result.db')
             case 'report-mini-pisa':
